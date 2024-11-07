1cf470dea8eaad304ac4e9b746a8a21e5d79979a
IT24104194 <IT24104194@my.sliit.lk>
2024-11-07 09:52:08 +0530

    Q1B

---
+++ Q1B/IT24104194Lab3Q1B.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class IT24104194Lab3Q1B{
+ 	public static void main (String[] args){
+		Scanner input=new Scanner(System.in);
+		System.out.print("Enter the price of 1kg rice :");
+		double value1=input.nextDouble();
+		System.out.print("Enter the number of kilograms you want to buy :");
+		double value2=input.nextDouble();
+
+
+	
+	double TotalAmount=value1*value2*90/100;
+	System.out.print("The Total amount is :"+TotalAmount);
+	
+}
+}
